18e62308330683e04609e95a576b092d471c942b
SoumyaKodumuri <soumyakodumuri6@gmail.com>
2024-10-01 11:29:43 +0530

    api added

---
+++ src/main/java/com/p2/microservice/ApiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.p2.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/p2/microservice/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p2.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
